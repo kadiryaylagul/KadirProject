262284d72adb9a82518cd41115a7369d6f4570eb
kadiryaylagul <76875074+kadiryaylagul@users.noreply.github.com>
2021-02-07 13:29:57 +0300

    First Task- added a message

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-
+        // I made some chances to my file that it is only that message
 
     }
 }
